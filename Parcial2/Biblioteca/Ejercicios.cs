73c0ec009c45ab81d7c8d7c9c305426da6f811ef
RJVasquez19 <47983895+RJVasquez19@users.noreply.github.com>
2019-03-19 19:51:05 -0600

    Add files via upload

---
+++ Parcial2/Biblioteca/Ejercicios.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Ejercicios
+    {
+        //Metodo Ejercicio 01
+        public void ejercicio01()
+        {
+            int num1, num2, resultado;
+            Console.WriteLine("\nIngrese el primer numero");
+            num1 = int.Parse(Console.ReadLine());
+
+            if (num1 == 0)
+            {
+                Console.WriteLine("El producto de 0 por cualquier numero es 0");
+            }
+            else
+            {
+                Console.WriteLine("Ingrese el segundo numero");
+                num2 = int.Parse(Console.ReadLine());
+
+                resultado = num1 * num2;
+                Console.WriteLine("Resultado: " + resultado);
+            }
+
+        }
+
+        //Metodo Ejercicio 02
+        public void ejercicio02() 
+        {
+            int num1, num2, resultado;
+            Console.WriteLine("Ingrese el primer numero");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo numero");
+            num2 = int.Parse(Console.ReadLine());
+
+            if (num2 == 0) 
+            {
+                Console.WriteLine("Error: no se puede dividir entre cero");
+            }
+            else 
+            {
+                resultado = num1 / num2;
+                Console.WriteLine("Resultado: " + resultado);
+            }
+        }
+
+        //Metodo Ejercicio 03
+        public void ejercicio03() 
+        {
+            string[] nombres = new string[5];
+
+            for (int i = 0; i < 5; i++) 
+            {
+                Console.WriteLine("\nIngrese el nombre para el alumno " + (i+1));
+                nombres[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("");
+            for (int i = 0; i < 5; i++) 
+            {
+                Console.WriteLine("El nombre " + (i+1) + "=" + nombres[i]);
+            }
+        }
+
+        public void ejercicio04()
+        {
+            int var=1, edad;
+            do
+            {
+                Console.WriteLine("\nIngrese la edad del estudiante " + var);
+                edad = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("\n**************************************");
+                Console.WriteLine("La edad del alumno " + var + " es: " + edad);
+                Console.WriteLine("**************************************");
+
+                var++;
+            }while(var <= 5);
+        }
+    }
+
+}
---
+++ Parcial2/Parcial2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biblioteca;
+
+namespace Parcial2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion;
+            do
+            {
+                Console.WriteLine("\nIngrese una opcion");
+                Console.WriteLine("1. MULTIPLICACION");
+                Console.WriteLine("2. DIVISION");
+                Console.WriteLine("3. CAPTURAR 5 NOMBRES");
+                Console.WriteLine("4. CAPTURAR 5 EDAD");
+                Console.WriteLine("5. SALIR");
+
+                opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        Ejercicios obj = new Ejercicios();
+                        obj.ejercicio01();
+                        break;
+                    case 2:
+                        Ejercicios obj1 = new Ejercicios();
+                        obj1.ejercicio02();
+                        break;
+                    case 3:
+                        Ejercicios obj2 = new Ejercicios();
+                        obj2.ejercicio03();
+                        break;
+                    case 4:
+                        Ejercicios obj3 = new Ejercicios();
+                        obj3.ejercicio04();
+                        break;
+                }
+
+
+            }while(opcion != 5);
+        }
+    }
+}
